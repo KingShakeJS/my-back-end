3ecbad256b33eb69035c3720fc7d01a2478e2e31
KingShakeJS <kingshakejs@yandex.ru>
2024-11-11 11:42:23 +0300

    test-3

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import express, {Request, Response} from 'express'
+
+const tls = require("tls");
 const app = express()
 const port =process.env.PORT || 3003
 const jsonBodyMiddleware = express.json()
@@ -5,4 +8,9 @@ app.use(jsonBodyMiddleware)
 
 app.get('/', (req: Request, res: Response) => {
     res.send('ddd')
+})
+
+
+app.listen(port, () => {
+    console.log(`сайтик стартанул на порте: ${port}`)
 })
