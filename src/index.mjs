a706c445ce38555fa841c37697fb3ad32b46aafb
KingShakeJS <kingshakejs@yandex.ru>
2024-11-11 12:10:49 +0300

    test-7

---
+++ src/index.mjs
@@ -0,0 +1,32 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+const authorize = ((req, res, next) => {
+
+    const auth = {login: process.env.USERNAME, password: process.env.PASSWORD}
+
+
+    const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+    const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':')
+
+
+    if (login && password && login === auth.login && password === auth.password) {
+
+        return next()
+    }
+
+    res.set('WWW-Authenticate', 'Basic realm="401"')
+    res.status(401).send('Authentication required.')
+});
+
+app.use('/', authorize);
+app.use('/', express.static('build'));
+
+app.listen(3000);
+console.log(`🚀 Server ready at http://localhost:3000`);
